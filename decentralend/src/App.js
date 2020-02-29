c2a03887684b515ed43e66601464d04c312e5c50
Dane <dane.reimers@gmail.com>
2020-02-28 22:42:12 -0800

    Added polypolls files

--- decentralend/src/App.js
+++ decentralend/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {
+  Route,
+  HashRouter,
+  NavLink,
+} from "react-router-dom";
+import Web3 from 'web3';
 import './App.css';
+import { ELECTION_MANAGER_ABI, ELECTION_MANAGER_ADDRESS } from './config.js'
+import Home from './Home';
+import CreateCircle from './CreateCircle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const electionManager = new web3.eth.Contract(ELECTION_MANAGER_ABI, ELECTION_MANAGER_ADDRESS)
+    this.setState({ electionManager })
+    let numElections = await electionManager.methods.getNumElections().call()
+    numElections = [] 
+    this.setState({ numElections })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '' }
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>Decentralend</h1>
+          <ul className="header">
+            <li><NavLink to="/home">Home</NavLink></li>
+            <li><NavLink to="/create-circle">Create Circle</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/home" render={props =>
+                <Home numElections={this.state.numElections} {...props} />
+              }
+            />
+            <Route exact path="/create-circle" render={props =>
+                <CreateCircle electionManager={this.state.electionManager} account={this.state.account} {...props} />
+              }
+            />
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
