dab89a73e9c08b3cd0c715f46ac5ce0dce416a59
Evan Morris <evancmorris17@gmail.com>
2020-02-29 13:53:51 -0800

    fixed top thing

--- decentralend/src/ViewGroup.js
+++ decentralend/src/ViewGroup.js
@@ -2,14 +2,6 @@ import React, { Component } from "react";
 import './App.css';
 
 class ViewGroup extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { lendingGroup }
-        this.state = { loading: false };
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
 
     render() {
         return (
