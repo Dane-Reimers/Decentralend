ae7590097bb921d0a89e66607bad7833c125dc39
Dylan Mooers <dmooers@calpoly.edu>
2020-02-28 23:09:51 -0800

    added request and pay

--- Contracts/contracts/LendingGroup.sol
+++ Contracts/contracts/LendingGroup.sol
@@ -10,15 +10,13 @@ contract LendingGroup {
 
   struct Member {
     string name;
-    string physAddress;
-    string phoneNumber;
+    int balance;
   }
 
   string name;
   address owner;
   address[] memberAddress;
   mapping (address => Member) members;
-  mapping (address => int) balances;
   mapping (address => Request) requests;
 
   event newRequest (
@@ -30,19 +28,27 @@ contract LendingGroup {
     owner = _owner;
   }
 
-  function addMember (address _memberAddress, string memory name,
+  function addMember (address _memberAddress, string memory _name,
                       string memory phoneNumber, string memory physAddress) public {
     memberAddress.push(_memberAddress);
-    members[_memberAddress] = Member(
-            name,
-            physAddress,
-            phoneNumber
-    );
-    balances[_memberAddress] = 0;
+    members[_memberAddress] = Member(_name, 0);
     requests[_memberAddress] = Request(0, 0);
   }
 
   function requestMoney (uint amount) public {
-    requests[msg.sender] = Request(amount, 0);
+    require (requests[msg.sender].requested == 0,
+     "You have already requested money");
+    requests[msg.sender] = Request(amount * 1000, 0);
+  }
+
+  function giveMoney (address member, int amountDonated) public payable {
+     require(
+      IERC20Token(circle.tokenAddress).transferFrom(
+        msg.sender,
+        address(this),
+        amountDonated
+      ),
+      "Transfer of contribution failed"
+    );
   }
 }
