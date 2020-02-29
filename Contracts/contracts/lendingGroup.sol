50174560e71820caa6061dc0b05bd1fe8c14cd05
Dylan Mooers <dmooers@calpoly.edu>
2020-02-28 22:18:40 -0800

    started base smart contract

---
+++ Contracts/contracts/lendingGroup.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.0;
+
+
+contract lendingGroup {
+  struct Request {
+    int requested;
+    int totalFulfilled;
+  }
+
+  address[] members;
+  string name;
+  address owner;
+  mapping (address => int) balances;
+  mapping (address => Request) requests;
+
+  event newRequest (
+    uint amountRequested
+  );
+
+  constructor(string memory _name) public {
+    name = _name;
+    owner = msg.sender;
+  }
+
+  function addMember (address member) public {
+    members.add(member);
+    balances[member] = 0;
+    requests[member] = Request(0, 0);
+  }
+
+  function requestMoney ()
+}
